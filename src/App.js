b6d86a1480d3439f5b5b72b7818a854bc3f4ca09
shivansh <shivanshrocks21@gmail.com>
2020-08-27 17:59:00 +0530

    Portfolio

---
+++ src/App.js
@@ -0,0 +1,320 @@
+import React from "react";
+// import logo from './logo.svg';
+import Portfolio from "./components/Portfolio";
+import "./App.css";
+
+function App() {
+  const portfolioLinks = [
+    {
+      title: "Healthcare",
+      caption: "Website",
+    },
+    {
+      title: "Ngo",
+      caption: "Web application",
+    },
+    {
+      title: "Daiting",
+      caption: "app",
+    },
+    {
+      title: "Cross",
+      caption: "Game",
+    },
+    {
+      title: "Fashion",
+      caption: "Website Design",
+    },
+    {
+      title: "Window",
+      caption: "Graphic",
+    },
+    {
+      title: "Brochure",
+      caption: "UI/UX",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <nav
+        className="navbar navbar-expand-lg navbar-dark fixed-top"
+        id="mainNav"
+      >
+        <div className="container">
+          <a className="navbar-brand js-scroll-trigger" href="#page-top"></a>
+          <button
+            className="navbar-toggler navbar-toggler-right"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            Menu
+            <i className="fa fa-bars"></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav text-uppercase ml-auto">
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#services">
+                  Skills
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#portfolio">
+                  Projects
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#about">
+                  About
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <header className="masthead">
+        <div className="container">
+          <div className="intro-text">
+            <div className="intro-lead-in">Portfolio</div>
+            <div className="intro-heading text-uppercase">
+              Shivansh Srivastava
+            </div>
+            <a
+              className="btn btn-primary btn-xl text-uppercase js-scroll-trigger"
+              href="#services"
+            >
+              Start
+            </a>
+          </div>
+        </div>
+      </header>
+
+      <section className="page-section" id="services">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <h2 className="section-heading text-uppercase">Skills</h2>
+              <h3 className="section-subheading text-muted">
+                Lorem ipsum dolor sit amet consectetur.
+              </h3>
+            </div>
+          </div>
+          <div className="row text-center">
+            <div className="col-md-4">
+              <span className="fa-stack fa-4x">
+                <i className="fa fa-circle fa-stack-2x text-primary"></i>
+                <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+              </span>
+              <h4 className="service-heading">Web Development</h4>
+              <p className="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+                maxime quam architecto quo inventore harum ex magni, dicta
+                impedit.
+              </p>
+            </div>
+            <div className="col-md-4">
+              <span className="fa-stack fa-4x">
+                <i className="fa fa-circle fa-stack-2x text-primary"></i>
+                <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
+              </span>
+              <h4 className="service-heading">Graphic Design</h4>
+              <p className="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+                maxime quam architecto quo inventore harum ex magni, dicta
+                impedit.
+              </p>
+            </div>
+            <div className="col-md-4">
+              <span className="fa-stack fa-4x">
+                <i className="fa fa-circle fa-stack-2x text-primary"></i>
+                <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
+              </span>
+              <h4 className="service-heading">Game Development</h4>
+              <p className="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+                maxime quam architecto quo inventore harum ex magni, dicta
+                impedit.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Portfolio portfolioLinks={portfolioLinks}></Portfolio>
+
+      <section className="py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3 col-sm-6">
+              <a href="#something">
+                <img
+                  className="img-fluid d-block mx-auto"
+                  src="img/logos/envato.jpg"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-md-3 col-sm-6">
+              <a href="#something">
+                <img
+                  className="img-fluid d-block mx-auto"
+                  src="img/logos/designmodo.jpg"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-md-3 col-sm-6">
+              <a href="#something">
+                <img
+                  className="img-fluid d-block mx-auto"
+                  src="img/logos/themeforest.jpg"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-md-3 col-sm-6">
+              <a href="#something">
+                <img
+                  className="img-fluid d-block mx-auto"
+                  src="img/logos/creative-market.jpg"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="page-section" id="contact">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <h2 className="section-heading text-uppercase">Contact Us</h2>
+              <h3 className="section-subheading text-muted">
+                Lorem ipsum dolor sit amet consectetur.
+              </h3>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-12">
+              <form id="contactForm" name="sentMessage" novalidate="novalidate">
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        className="form-control"
+                        id="name"
+                        type="text"
+                        placeholder="Your Name *"
+                        required="required"
+                        data-validation-required-message="Please enter your name."
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                    <div className="form-group">
+                      <input
+                        className="form-control"
+                        id="email"
+                        type="email"
+                        placeholder="Your Email *"
+                        required="required"
+                        data-validation-required-message="Please enter your email address."
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                    <div className="form-group">
+                      <input
+                        className="form-control"
+                        id="phone"
+                        type="tel"
+                        placeholder="Your Phone *"
+                        required="required"
+                        data-validation-required-message="Please enter your phone number."
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <textarea
+                        className="form-control"
+                        id="message"
+                        placeholder="Your Message *"
+                        required="required"
+                        data-validation-required-message="Please enter a message."
+                      ></textarea>
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                  <div className="clearfix"></div>
+                  <div className="col-lg-12 text-center">
+                    <div id="success"></div>
+                    <button
+                      id="sendMessageButton"
+                      className="btn btn-primary btn-xl text-uppercase"
+                      type="submit"
+                    >
+                      Send Message
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="footer">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-4">
+              <span className="copyright">Copyright &copy;</span>
+            </div>
+            <div className="col-md-4">
+              <ul className="list-inline social-buttons">
+                <li className="list-inline-item">
+                  <a href="#something">
+                    <i className="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#something">
+                    <i className="fa fa-facebook-f"></i>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#something">
+                    <i className="fa fa-linkedin-in"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-4">
+              <ul className="list-inline quicklinks">
+                <li className="list-inline-item">
+                  <a href="#something">Privacy Policy</a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#something">Terms of Use</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
